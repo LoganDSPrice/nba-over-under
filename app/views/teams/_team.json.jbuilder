0693026decb59c409ace5fef6db061c1569ec665
Logan Price <logandsprice@users.noreply.github.com>
2018-11-08 16:57:38 -0600

    Migration - Teams

---
+++ app/views/teams/_team.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! team, :id, :nba_id, :city, :name, :conference, :division, :line, :created_at, :updated_at
+json.url team_url(team, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_08_225356) do
+ActiveRecord::Schema.define(version: 2018_11_08_225722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,17 @@ ActiveRecord::Schema.define(version: 2018_11_08_225356) do
   create_table "contestants", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "nba_id"
+    t.string "city"
+    t.string "name"
+    t.string "conference"
+    t.string "division"
+    t.float "line"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
