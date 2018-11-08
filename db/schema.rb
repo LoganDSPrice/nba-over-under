ae850910e91fb00c10a35137dd971cf07d646f73
Logan Price <logandsprice@users.noreply.github.com>
2018-11-08 16:58:55 -0600

    Migration - Picks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_08_225722) do
+ActiveRecord::Schema.define(version: 2018_11_08_225828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2018_11_08_225722) do
   create_table "contestants", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "picks", force: :cascade do |t|
+    t.integer "contestant_id"
+    t.integer "team_id"
+    t.boolean "over"
+    t.boolean "lock"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
