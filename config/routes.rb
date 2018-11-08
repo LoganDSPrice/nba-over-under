2a4ae7b0f6d3d4ddab5905c6a0238d733923bff3
Logan Price <logandsprice@users.noreply.github.com>
2018-11-08 17:00:30 -0600

    Cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root "contestants#index"
+
+  resources :contestants
   resources :picks
   resources :teams
-  resources :contestants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "application#index"
 end
