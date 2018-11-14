9f021b304ea2da0f81c198b401a50cbfe8ed3670
Logan Price <logandsprice@users.noreply.github.com>
2018-11-14 16:32:10 -0600

    Use httparty to grab teams from NBA API as opposed to saved json file

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,10 +36,14 @@ contestants.each do |c|
   contestant.save if contestant.valid?
 end
 
-team_file = File.read("teams.json")
-teams_json = JSON.parse(team_file)
+url = "http://data.nba.net/10s/prod/v2/2018/teams.json"
+response = HTTParty.get(url)
+parsed = response.parsed_response
+nba_teams = parsed["league"]["standard"].select {|t| t["isNBAFranchise"] == true }
 
-nba_teams = teams_json["league"]["standard"].select {|t| t["isNBAFranchise"] == true }
+# team_file = File.read("teams.json")
+# teams_json = JSON.parse(team_file)
+# nba_teams = teams_json["league"]["standard"].select {|t| t["isNBAFranchise"] == true }
 
 Team.destroy_all
 nba_teams.each do |t|
