080cf11dfd2620fc3b789cda4c9a39e6f511497d
Logan Price <logandsprice@users.noreply.github.com>
2018-11-15 16:04:50 -0600

    Sample mailer preview working

--- app/mailers/nba_mailer.rb
+++ app/mailers/nba_mailer.rb
@@ -1,2 +1,10 @@
 class NbaMailer < ApplicationMailer
+  default from: "logandsprice@gmail.com"
+
+  def sample_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Sample Email')
+
+  end
+
 end
--- test/mailers/previews/nba_mailer_preview.rb
+++ test/mailers/previews/nba_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/nba_mailer
 class NbaMailerPreview < ActionMailer::Preview
-
+  def sample_mail_preview
+    NbaMailer.sample_email(Contestant.last)
+  end
 end
