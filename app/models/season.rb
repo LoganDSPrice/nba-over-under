b631150aef1f36ed39be027fece651342e2c59b4
Logan Price <logandsprice@gmail.com>
2021-10-15 16:14:53 -0500

    Create SeasonLines after Season creation

--- app/models/season.rb
+++ app/models/season.rb
@@ -6,4 +6,10 @@ class Season < ApplicationRecord
   has_many :locks, through: :picks
 
   validates_uniqueness_of :year
+
+  after_create :create_season_lines
+
+  def create_season_lines
+    Team.all.each { |team| season_lines.create(team: team)}
+  end
 end
