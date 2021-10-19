646a5815261d5feee6437eaa5d2bbb6eeff1eb00
Logan Price <logandsprice@gmail.com>
2021-10-19 11:52:33 -0500

    Generate SeasonChannel

---
+++ app/channels/season_channel.rb
@@ -0,0 +1,9 @@
+class SeasonChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "season_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
